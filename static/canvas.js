57d1da73dc2dfa78ee360778b46169bfd9e15a6a
Erick Fernando Cobo <fcobo95@gmail.com>
2017-04-14 10:03:45 -0600

    Se agrega funcionalidad al pong. Ya la bola rebota del lado del jugador, pero del lado del AI se queda pegado en el paddle. TODO.

--- static/canvas.js
+++ static/canvas.js
@@ -9,8 +9,8 @@ var animate = window.requestAnimationFrame ||
     };
 
 var canvas = document.createElement('canvas');
-var width = window.innerWidth - 500;
-var height = window.innerHeight - 500;
+var width = window.innerWidth - 400;
+var height = window.innerHeight - 300;
 canvas.width = width;
 canvas.height = height;
 canvas.border = 1;
@@ -23,6 +23,7 @@ window.onload = function () {
 };
 
 var update = function () {
+    ball.update(player.paddle, computer.paddle)
 };
 
 var player = new Player();
@@ -35,6 +36,7 @@ var render = function () {
     player.render();
     computer.render();
     ball.render();
+    ball.update(player.paddle, computer.paddle);
 };
 
 var step = function () {
@@ -58,7 +60,7 @@ Paddle.prototype.render = function () {
 };
 
 function Player() {
-    this.paddle = new Paddle(width - (width-20), ((height / 2) - 50), 20, 100);
+    this.paddle = new Paddle(width - (width - 20), ((height / 2) - 50), 20, 100);
 }
 
 function Computer() {
@@ -76,8 +78,8 @@ Computer.prototype.render = function () {
 function Ball(x, y) {
     this.x = x;
     this.y = y;
-    this.x_speed = 0;
-    this.y_speed = 3;
+    this.x_speed = -3;
+    this.y_speed = 0;
     this.radius = 10;
 }
 
@@ -86,6 +88,46 @@ Ball.prototype.render = function () {
     context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
     context.fillStyle = "#FFFFFF";
     context.fill();
+};
+
+Ball.prototype.update = function (paddle1, paddle2) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.y + 20;
+    var top_y = this.x + 20;
+    var bottom_x = this.y - 20;
+    var bottom_y = this.x - 20;
+
+    if (this.x - 50 < 0) { // hitting the left wall
+        this.x = 50;
+        this.x_speed = -this.x_speed;
+    } else if (this.x + 50 > width) { // hitting the right wall
+        this.x = width - 50;
+        this.y_speed = -this.y_speed;
+    }
+
+    if (this.y < 0 || this.y > height) { // a point was scored
+        this.x_speed = 0;
+        this.y_speed = 3;
+        this.x = height / 2;
+        this.y = width / 2;
+    }
+
+    if (top_y > height/2) {
+        if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+            // hit the player's paddle
+            this.y_speed = 3;
+            this.x_speed += (paddle1.x_speed / 2);
+            this.y += this.y_speed;
+        }
+    } else {
+        if (top_y < (paddle2.x + paddle2.height) && bottom_y > paddle2.x && top_x < (paddle2.y + paddle2.width) && bottom_x > paddle2.y) {
+            // hit the computer's paddle
+            this.y_speed = -3;
+            this.x_speed += (paddle2.x_speed / 2);
+            this.y += this.y_speed;
+        }
+    }
 };
 
 
