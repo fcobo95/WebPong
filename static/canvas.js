0b60fec9a5940ef9a7728fabef138c81229fbbc0
Erick Fernando Cobo <fcobo95@gmail.com>
2017-04-14 08:36:03 -0600

    Se hacen los recursos que se pintan el canvas. Ademas de esto, hago que el canvas se haga responsivo al tipo de pantalla. Se crea la paleta, la bola y se hacen los calculos necesarios para ubicarlos en el canvas de manera adecuada.

--- static/canvas.js
+++ static/canvas.js
@@ -9,12 +9,12 @@ var animate = window.requestAnimationFrame ||
     };
 
 var canvas = document.createElement('canvas');
-var width = 1500;
-var height = 750;
+var width = window.innerWidth - 500;
+var height = window.innerHeight - 500;
 canvas.width = width;
 canvas.height = height;
 canvas.border = 1;
-canvas.borderColor= 'white';
+canvas.borderColor = 'white';
 var context = canvas.getContext('2d');
 
 window.onload = function () {
@@ -22,16 +22,70 @@ window.onload = function () {
     animate(step);
 };
 
-var step = function() {
-  update();
-  render();
-  animate(step);
+var update = function () {
 };
 
-var update = function() {
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball((width / 2), (height / 2));
+
+var render = function () {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, width, height);
+    player.render();
+    computer.render();
+    ball.render();
 };
 
-var render = function() {
-  context.fillStyle = "#000";
-  context.fillRect(0, 0, width, height);
+var step = function () {
+    update();
+    render();
+    animate(step);
 };
+
+function Paddle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.x_speed = 0;
+    this.y_speed = 0;
+}
+
+Paddle.prototype.render = function () {
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+function Player() {
+    this.paddle = new Paddle(width - (width-20), ((height / 2) - 50), 20, 100);
+}
+
+function Computer() {
+    this.paddle = new Paddle(width - 40, height / 2 - 50, 20, 100);
+}
+
+Player.prototype.render = function () {
+    this.paddle.render();
+};
+
+Computer.prototype.render = function () {
+    this.paddle.render();
+};
+
+function Ball(x, y) {
+    this.x = x;
+    this.y = y;
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.radius = 10;
+}
+
+Ball.prototype.render = function () {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+    context.fillStyle = "#FFFFFF";
+    context.fill();
+};
+
+
